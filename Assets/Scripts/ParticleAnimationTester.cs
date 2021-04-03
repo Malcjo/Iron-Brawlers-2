ba3e1583ede42db4e09cccee66757ad54438d0eb
Peter <mcdermpete@gmail.com>
2021-04-03 18:29:15 +1300

    Created particle tester scene

---
+++ Assets/Scripts/ParticleAnimationTester.cs
@@ -0,0 +1,52 @@
+﻿using System.CodeDom;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleAnimationTester : MonoBehaviour
+{
+    [SerializeField] Animator anim;
+    // Start is called before the first frame update
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            anim.Play("JAB");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            anim.Play("HEAVY");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            anim.Play("SWEEP");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            anim.Play("AERIAL");
+        }
+        if (Input.GetKey(KeyCode.Alpha5))
+        {
+            anim.Play("RUN");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            anim.Play("DASH");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha7))
+        {
+            anim.Play("UP_AIR");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha8))
+        {
+            anim.Play("BACK_AIR");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha9))
+        {
+            anim.Play("ARMOUR_BREAK");
+        }
+    }
+}
