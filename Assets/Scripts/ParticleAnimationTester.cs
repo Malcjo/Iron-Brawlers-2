e0bdbe0c12051eb0b3bda3908f022544e4779af2
dante <dantekennedygeorge@outlook.com>
2021-04-04 17:46:51 +1200

    Particles

--- Assets/Scripts/ParticleAnimationTester.cs
+++ Assets/Scripts/ParticleAnimationTester.cs
@@ -8,6 +8,9 @@ public class ParticleAnimationTester : MonoBehaviour
     [SerializeField] Animator anim;
     [SerializeField] ParticleSystem Heavy;
     [SerializeField] ParticleSystem Sweap;
+    [SerializeField] ParticleSystem Jab;
+    [SerializeField] ParticleSystem Dash;
+    [SerializeField] ParticleSystem FowardAir;
     // Start is called before the first frame update
 
 
@@ -17,6 +20,7 @@ public class ParticleAnimationTester : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             anim.Play("JAB");
+            Jab.Play();
         }
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
@@ -32,6 +36,7 @@ public class ParticleAnimationTester : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Alpha4))
         {
             anim.Play("AERIAL");
+            FowardAir.Play();
         }
         if (Input.GetKey(KeyCode.Alpha5))
         {
@@ -40,6 +45,7 @@ public class ParticleAnimationTester : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Alpha6))
         {
             anim.Play("DASH");
+            Dash.Play();
         }
         if (Input.GetKeyDown(KeyCode.Alpha7))
         {
